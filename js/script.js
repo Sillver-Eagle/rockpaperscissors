bf13f2b1c196f87d93c9075d2575bcd8def746f8
Glitch (debonair-gem-baron) <none>
2021-02-02 21:47:39 +0000

    🎇🛤 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,6 +4,52 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+let scissors;
+let rock;
+let paper;
 
+
+var computerChoice = Math.random();
+
+
+
+$("#shoot").click(function() {
+  let input = $("#input").val();
+  let computerChoice = $("#computerChoice").val();
+  //when user picks rock
+  
+  if (computerChoice > .8){
+    (input === rock && computerChoice <= 0.40) 
+console.log("Tie"); }
+  
+   else if (input === rock && computerChoice >= 0.60) {
+    console.log("Loss");
+}
+   else if (input === rock && computerChoice === 0.50) { 
+    console.log("Win");
+  }
+  //when user picks Paper
+ else  if (input === paper && computerChoice <= 0.40) {
+    console.log("Win");
+ }
+  else if (input === paper && computerChoice >= 0.60) { 
+    console.log("Tie");
+}
+  else if  (input === paper && computerChoice === 0.50) {
+    console.log("Loss");
+  }
+  //when user picks scissors
+
+  else if (input === scissors && computerChoice <= 0.40) {
+    console.log("Loss");
+}
+   else if (input === scissors && computerChoice >= 0.60) {
+    console.log("Win");
+}
+
+   else if (input === scissors && computerChoice === 0.50) {
+    console.log("Tie");
+  }
+});
 
 
